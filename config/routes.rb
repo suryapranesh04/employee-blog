502c50018ba6c996c975fbb58623b38e60a96454
Surya <surya@codingmart.com>
2019-03-12 19:30:37 +0530

    updated app

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+	root 'employees#index'
+
+	get 'employees/about'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
